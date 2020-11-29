c8c4f2a32bb68cb381337be594a72e4288ec15e3
Kekalainen <git@kekalainen.me>
2020-11-29 02:49:01 +0200

    Controller-level RequestMapping

--- src/main/java/projekti/UserController.java
+++ src/main/java/projekti/UserController.java
@@ -7,25 +7,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-    @GetMapping("/api/users/{slug}")
+    @GetMapping("/{slug}")
     public User show(@PathVariable("slug") String slug) {
         return userService.findBySlug(slug);
     }
 
-    @PostMapping("/api/users/login")
+    @PostMapping("/login")
     public User login(@RequestBody User user) {
         return userService.login(user);
     }
 
-    @PostMapping("/api/users/signup")
+    @PostMapping("/signup")
     public User signup(@Valid @RequestBody User user) {
         return userService.signup(user);
     }
