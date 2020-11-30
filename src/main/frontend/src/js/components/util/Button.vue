ea404f504f46c78a66ae740d290df0f9b9330957
Kekalainen <git@kekalainen.me>
2020-11-30 22:47:58 +0200

    Add secondary button style

--- src/main/frontend/src/js/components/util/Button.vue
+++ src/main/frontend/src/js/components/util/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button @click="$emit('click')" class="py-2 px-4 bg-purple-500 text-white font-semibold hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75 rounded-md shadow-md">
+    <button @click="$emit('click')" class="py-2 px-4 bg-purple-500 text-white font-semibold hover:bg-purple-700 border focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75 rounded-md shadow-md">
         <slot />
     </button>
 </template>
