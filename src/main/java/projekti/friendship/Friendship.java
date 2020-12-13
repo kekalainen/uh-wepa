a9283746518444f8b1997afa97d8566558f81765
Kekalainen <git@kekalainen.me>
2020-12-13 23:14:33 +0200

    Add unique constraint for friendship

--- src/main/java/projekti/friendship/Friendship.java
+++ src/main/java/projekti/friendship/Friendship.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.jpa.domain.AbstractPersistable;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 import projekti.user.User;
 
 @Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"requester_id", "requestee_id"})})
 @AllArgsConstructor @Data @NoArgsConstructor
 public class Friendship extends AbstractPersistable<Long> {
 
